4e452d2e68a8a48d84b4c02c33adcfa8612da198
owen <1053210246@qq.com>
2019-04-08 11:24:23 +0800

    Tiled map test example

---
+++ assets/tilemap/testTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="testTileset" tilewidth="60" tileheight="60" tilecount="16" columns="4" margin="0" spacing="0">
+ <image source="hexa.png" width="240" height="180"/>
+</tileset>
