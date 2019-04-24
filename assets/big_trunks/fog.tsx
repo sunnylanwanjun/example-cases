33b3a529bd03d2d1b63489ebf2aa1d8a9f694bc2
owen <1053210246@qq.com>
2019-04-24 19:35:26 +0800

    Tiled map res

---
+++ assets/big_trunks/fog.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="fog" tilewidth="128" tileheight="64" tilecount="1" columns="1">
+ <image source="fog.png" width="128" height="64"/>
+</tileset>
